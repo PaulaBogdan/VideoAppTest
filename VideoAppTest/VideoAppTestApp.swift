6c9fd5fd31e3b58e50613234cf74c8cc0b8ee2ea
Paula <paula.kupidlowska@gmail.com>
2023-01-12 10:17:22 +0100

    Initial Commit

---
+++ VideoAppTest/VideoAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoAppTestApp.swift
+//  VideoAppTest
+//
+//  Created by Paula on 12/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct VideoAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
